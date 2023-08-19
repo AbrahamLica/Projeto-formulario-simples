3f6df0377afcda342c386d555cb5786d355e2c86
Abraham Melquisedeque Pereira Licá <103894033+AbrahamLica@users.noreply.github.com>
2023-08-19 20:46:53 -0300

    more funtions

--- script.js
+++ script.js
@@ -7,24 +7,44 @@ const spanCpf = document.querySelector(".spanCpf");
 const spanEmail = document.querySelector(".spanEmail");
 const spanAge = document.querySelector(".spanAge");
 const buttonSend = document.querySelector(".ButtonSend");
+const select = document.querySelector("#select");
+const inputsCheckbox = document.querySelectorAll(".input-checkbox");
+/////////////////////////////////////////////////////////////////////////
+const containerModal = document.querySelector(".containerModal");
+const nameModal = document.querySelector(".nameModal");
+const cpfModal = document.querySelector(".cpfModal");
+const emailModal = document.querySelector(".emailModal");
+const ageModal = document.querySelector(".ageModal");
+const professionModal = document.querySelector(".professionModal");
+const qualitiesModal = document.querySelector(".qualitiesModal");
+const observationsModal = document.querySelector(".observationsModal");
+/////////////////////////////////////////////////////////////////////////
+var inputNameValid = false;
+var inputAgeValid = false;
+var inputEmailValid = false;
+var inputCpfValid = false;
+var inputSelectOptValid = false;
+var qualities = []
 
 function validateInputName() {
   //////////////////Input Name///////////////////////////////////////////////////////
+
   if (!name.value) {
     spanName.innerHTML = "O campo nome não pode estar vazio!";
     spanName.style.display = "block";
-    name.focus();
+    inputNameValid = false;
   } else if (name.value.length >= 1 && name.value.length < 3) {
     spanName.innerHTML = "O campo nome não pode ter menos de 3 caracteres!!";
     spanName.style.display = "block";
-    name.focus();
+    inputNameValid = false;
   } else if (containsSpecialChars(name.value) == true) {
     spanName.innerHTML = "O campo nome não pode ter caracteres especiais";
     spanName.style.display = "block";
-    name.focus();
+    inputNameValid = false;
   } else {
     spanName.innerHTML = "";
     spanName.style.display = "none";
+    inputNameValid = true;
   }
 }
 
@@ -33,43 +53,51 @@ function validateInputAge() {
   if (!age.value) {
     spanAge.innerHTML = "O campo Idade está vazio!";
     spanAge.style.display = "block";
-    age.focus();
+    inputAgeValid = false;
+  } else {
+    spanAge.innerHTML = "";
+    spanAge.style.display = "none";
+    inputAgeValid = true;
   }
 }
 
 function validateInputEmail() {
   ////////////////// Input Email ///////////////////////////////////////////////////////
+
   if (!email.value) {
     spanEmail.innerHTML = "O campo Email está vazio!";
     spanEmail.style.display = "block";
-    email.focus();
-  } else if (email.value.length < 4) {
-    spanEmail.innerHTML = "O campo Email não pode ter menos de 4 caracteres!";
+    inputEmailValid = false;
+  } else if (email.value.length < 6) {
+    spanEmail.innerHTML = "O campo Email não pode ter menos de 6 caracteres!";
     spanEmail.style.display = "block";
-    email.focus();
+    inputEmailValid = false;
   } else if (validateEmail(email.value) == false) {
     spanEmail.innerHTML = "O campo Email não está seguindo o padrão correto.";
     spanEmail.style.display = "block";
-    email.focus();
+    inputEmailValid = false;
   } else {
     spanEmail.innerHTML = "";
     spanEmail.style.display = "none";
+    inputEmailValid = true;
   }
 }
 
 function validateInputCpf() {
   ////////////// Input Cpf ///////////////////////////////////////////////////////
+
   if (!cpf.value) {
     spanCpf.innerHTML = "O campo Cpf não pode estar vazio!";
     spanCpf.style.display = "block";
-    cpf.focus();
+    inputCpfValid = false;
   } else if (cpf.value.length >= 1 && cpf.value.length < 11) {
     spanCpf.innerHTML = "O campo Cpf não pode ter menos de 11 caracteres!";
     spanCpf.style.display = "block";
-    cpf.focus();
+    inputCpfValid = false;
   } else {
     spanCpf.innerHTML = "";
     spanCpf.style.display = "none";
+    inputCpfValid = true;
   }
 }
 
@@ -87,22 +115,82 @@ function validateEmail(str) {
 }
 
 buttonSend.addEventListener("click", (e) => {
+
   e.preventDefault();
-  console.log('enviou!')
+
+  // checar se o dropdown tem alguma opção selecionada
+  if (select.selectedIndex == 0) {
+    alert("Selecione sua profissão");
+    inputSelectOptValid = false;
+  } else {
+    inputSelectOptValid = true;
+  }
+
+  //checar se tem algum checkbox de qualidade marcado
+  for (let i = 0; i < inputsCheckbox.length; i++) {
+    if (inputsCheckbox[i].checked) {
+      qualities.push(inputsCheckbox[i].value)
+    }
+  }
+
+  if (
+    inputNameValid &&
+    inputEmailValid &&
+    inputAgeValid &&
+    inputCpfValid &&
+    inputSelectOptValid
+  ) {
+    containerModal.style.display = 'flex'
+
+    nameModal.innerHTML = name.value;
+    cpfModal.innerHTML = cpf.value;
+    emailModal.innerHTML = email.value;
+    ageModal.innerHTML = age.value;
+    professionModal.innerHTML = select.options[select.selectedIndex].text;
+    qualitiesModal.innerHTML = qualities.join(', ')
+    // observationsModal.innerHTML =
+  } else {
+    console.log("falta");
+  }
 });
 
 name.addEventListener("blur", () => {
   validateInputName();
 });
 
-age.addEventListener("blur", () => {
-  validateInputAge();
+cpf.addEventListener("blur", () => {
+  validateInputCpf();
 });
 
 email.addEventListener("blur", () => {
   validateInputEmail();
 });
 
-cpf.addEventListener("blur", () => {
-  validateInputCpf();
+age.addEventListener("blur", () => {
+  validateInputAge();
+});
+
+name.addEventListener("keyup", () => {
+  if (name.value.length > 2) {
+    validateInputName();
+  }
+});
+
+cpf.addEventListener("keyup", () => {
+  if (cpf.value.length > 10) {
+    validateInputCpf();
+  }
+});
+
+email.addEventListener("keyup", () => {
+  if (email.value.length > 5) {
+    validateInputEmail();
+  }
+});
+
+age.addEventListener("keyup", () => {
+  if (age.value.length >= 1) {
+    validateInputAge();
+    console.log("teste");
+  }
 });
